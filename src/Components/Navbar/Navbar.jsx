10e9c5ed06f4798c3d496aa4520ed469a124b628
Shaivaan <93570605+Shaivaan@users.noreply.github.com>
2022-04-07 23:20:06 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import { Link, useNavigate } from "react-router-dom"
+
+export const Navbar = ()=>{
+   
+    return <div>
+        <Link to = "/"><button>Home</button></Link>
+        <Link to = "/form"><button>Form</button></Link>
+    </div>
+}
